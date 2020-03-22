4ffb38fc991721567e4804e81494ce37be66bd53
Brian Schroeder <brian.schroeder@gmx.de>
2020-03-22 22:34:42 +0100

    brsc FTI-1.0.0 optimize configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -731,7 +731,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 76, 54, 214, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 72, 50, 218, 96 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -751,7 +751,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 100, 100, 100, 100 }
+#define DEFAULT_MAX_ACCELERATION      { 100, 100, 100, 1000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
