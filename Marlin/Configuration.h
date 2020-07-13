ba746ca8709fa89364ee122b5c9dfd0f57f39ee1
Brian Schroeder <brian.schroeder@gmx.de>
2020-07-13 17:34:31 +0200

    brsc FTI-2.0.0 mechanics

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1056,7 +1056,7 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
+#define INVERT_X_DIR true
 #define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 
@@ -1092,8 +1092,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 230
-#define Y_BED_SIZE 135
+#define X_BED_SIZE 240
+#define Y_BED_SIZE 130
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
