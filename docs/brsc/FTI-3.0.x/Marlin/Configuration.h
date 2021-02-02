2ea58ccd23086125af571a90a1a033bdf147c5de
Brian Schroeder <brian.schroeder@gmx.de>
2021-02-02 16:49:56 +0100

    brsc FTI-3.0.1 optimize print bed, increase Y printspace

--- docs/brsc/FTI-3.0.x/Marlin/Configuration.h
+++ docs/brsc/FTI-3.0.x/Marlin/Configuration.h
@@ -1123,7 +1123,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 255
-#define Y_BED_SIZE 170
+#define Y_BED_SIZE 185
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1131,7 +1131,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 170
+#define Z_MAX_POS 185
 
 /**
  * Software Endstops
