b6754acba2a7d6cba737369be638abf2445e3693
Brian Schroeder <brian.schroeder@gmx.de>
2023-01-06 12:13:13 +0100

    2.1.x-brsc-FTI: refactoring - improve dimensions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1724,8 +1724,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 260
-#define Y_BED_SIZE 190
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 210
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1733,7 +1733,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 170
+#define Z_MAX_POS 180
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
