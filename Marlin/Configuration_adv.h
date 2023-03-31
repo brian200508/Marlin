64a8782bbfa3577dfd2f259a13cd5bf9806f03c4
Brian Schroeder <brian.schroeder@gmx.de>
2023-03-31 21:14:46 +0200

    brsc-FTI fix X-axis direction, fix FAN speed

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -577,8 +577,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-#define FAN_MIN_PWM 96
-#define FAN_MAX_PWM 224
+#define FAN_MIN_PWM 233
+#define FAN_MAX_PWM 255
 
 /**
  * Fan Fast PWM
