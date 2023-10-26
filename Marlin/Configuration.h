3999b3cd970f797b0f10565d79b966bf1f9700b9
Brian Schroeder <brian.schroeder.bs@gmail.com>
2023-10-26 23:18:07 +0200

    brsc-FTI fischertechnik 3D Printer mod

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1196,7 +1196,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1341, 94 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 667, 667, 1341, 94 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
@@ -1671,7 +1671,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
@@ -1683,7 +1683,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
