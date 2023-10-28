723832f7a06c38f130c4557a584c59a3c189605a
Brian Schroeder <brian.schroeder.bs@gmail.com>
2023-10-29 00:23:00 +0200

    brsc-FTI fischertechnik 3D Printer mod

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1694,8 +1694,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 300
-#define Y_BED_SIZE 210
+#define X_BED_SIZE 186
+#define Y_BED_SIZE 258
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
