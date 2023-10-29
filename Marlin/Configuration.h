a711e334177812dd3d7b6965ad5495f24d9020d4
Brian Schroeder <brian.schroeder.bs@gmail.com>
2023-10-29 21:30:43 +0100

    brsc-FTI fischertechnik 3D Printer mod

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1175,11 +1175,11 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 100, 100, 5, 10 }
+#define DEFAULT_MAX_FEEDRATE          { 100, 100, 5, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 400, 400, 10, 20 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 400, 400, 10, 50 } // ...or, set your own edit limits
 #endif
 
 /**
